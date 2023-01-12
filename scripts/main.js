de87d53cdd66b7058fef7211cbf85bd7cb88f982
Sergey Gusterev <copywriter-sg@yandex.ru>
2023-01-12 21:17:17 +0300

    close menu overlay esc

--- scripts/main.js
+++ scripts/main.js
@@ -4,10 +4,33 @@ import "normalize.css";
 const burger = document.querySelector(".burger");
 const mobileMenu = document.querySelector(".mobile-menu");
 
-const openMenu = (item, className) => {
-  item.classList.toggle(className);
+const toggleMenu = (menu) => {
+  menu.classList.toggle("mobile-menu_active");
+  document.addEventListener("keydown", handleEscClose);
+  if (!menu.classList.contains("mobile-menu_active")) {
+    document.removeEventListener("keydown", handleEscClose);
+  }
 };
 
-burger.addEventListener("click", () => {
-  openMenu(mobileMenu, "mobile-menu_active");
+const handleEscClose = (event) => {
+  if (event.key === "Escape") {
+    const openMenu = document.querySelector(".mobile-menu_active");
+    toggleMenu(openMenu);
+  }
+};
+
+document.addEventListener("click", (e) => {
+  const target = e.target;
+  const isMenu = target === mobileMenu || mobileMenu.contains(target);
+  const isBurger = target === burger;
+  const menuIsActive = mobileMenu.classList.contains("mobile-menu_active");
+
+  if (!isMenu && !isBurger && menuIsActive) {
+    toggleMenu(mobileMenu);
+  }
+});
+
+burger.addEventListener("click", (e) => {
+  e.stopPropagation();
+  toggleMenu(mobileMenu);
 });
