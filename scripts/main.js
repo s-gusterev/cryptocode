2f704999b262ce08021ccbbd954d96cbcb0ba8c6
Sergey Gusterev <copywriter-sg@yandex.ru>
2023-01-12 15:47:57 +0300

    header mobile menu

--- scripts/main.js
+++ scripts/main.js
@@ -1,2 +1,13 @@
 import "/css/style.css";
 import "normalize.css";
+
+const burger = document.querySelector(".burger");
+const mobileMenu = document.querySelector(".mobile-menu");
+
+const openMenu = (item, className) => {
+  item.classList.toggle(className);
+};
+
+burger.addEventListener("click", () => {
+  openMenu(mobileMenu, "mobile-menu_active");
+});
